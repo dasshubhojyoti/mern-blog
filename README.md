4f4139e938795102e8acb0daab51acd1fdf56da5
Shubhojyoti Das <dasshubhojyoti97@gmail.com>
2023-06-23 16:30:20 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Available Scripts
+
+In the project directory,
+
+Go to the api directory and run:
+
+### `npm start`
+
+Again go to the client directory and run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./login.css";
+import { useState } from 'react';
+
+export default function Login() {
+  const [errorMessage, setErrorMessage] = useState('');
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+      setErrorMessage("Wrong Credentials"); // Updating the state variable with the error message
+    }
+  };
+
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      {errorMessage && <p style={{ color: 'red', fontWeight: 'bold' }}>{errorMessage}</p>}
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter your username..."
+          ref={userRef}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="Enter your password..."
+          ref={passwordRef}
+        />
+        <button className="loginButton" type="submit" disabled={isFetching}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
